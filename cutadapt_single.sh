665d7b652707cfaf591fda27a3b7a056e84afe1e
Holly Paden <paden.10@osu.edu>
2021-02-20 11:14:26 -0500

    drafted cutadapt script to include date and 4 arguments

--- cutadapt_single.sh
+++ cutadapt_single.sh
@@ -5,3 +5,19 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 set -e -u -o pipefail
+
+#Add date
+date
+
+#Let script accept 4 arguments that can be passed to it on the command-line
+forward_fastq=
+output_directory=results_trim
+forward_primer=GAGTG[CT]CAGC[AC]GCCGCGGTAA
+reverse_primer=TTACCGCGGC[GT]GCTG[AG]CACTC  
+
+forward_fastq="$1"
+output_directory="$2"
+forward_primer="$3"
+reverse_primer="$4"
+
+#Check that arguments are properly provided
