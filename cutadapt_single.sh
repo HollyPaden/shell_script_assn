988da973c6d69100c2763d183d78694e21f129d4
Holly Paden <paden.10@osu.edu>
2021-02-20 14:08:30 -0500

    quality control messages added to script

--- cutadapt_single.sh
+++ cutadapt_single.sh
@@ -10,21 +10,24 @@ set -e -u -o pipefail
 date
 
 #Let script accept 4 arguments that can be passed to it on the command-line
-forward_fastq=fastq/201-S4-V4-V5_S53_L001_R1_001.fastq
-output_directory=results_trim
+forward_fastq=./201-S4-V4-V5_S53_L001_R1_001.fastq
+output_directory=./results_trim
 forward_primer="GAGTG[CT]CAGC[AC]GCCGCGGTAA"
 reverse_primer="TTACCGCGGC[GT]GCTG[AG]CACTC"  
 
-forward_fastq="$1"
-output_directory="$2"
-forward_primer="$3"
-reverse_primer="$4"
+#forward_fastq="$1"
+#output_directory="$2"
+#forward_primer="$3"
+#reverse_primer="$4"
 
 # Create and assign output directory
-mkdir -p $output_directory
+mkdir -p "$output_directory"
+
+#Message to indicate the script has initiated
+echo "Script running...please wait"
 
 #Check whether the FASTQ file exists, if it is a regular file, and if it can be read
-if [ ! -f "$forward_fastq" ] || [ ! -r "$forward_fastq" ] || [ ! -e "$fprward_fastq" ]; then
+if [ ! -f "$forward_fastq" ] || [ ! -r "$forward_fastq" ] || [ ! -e "$forward_fastq" ]; then
 
     echo "Error: either is not file, does not exist, or cannot be read"
     echo "You provided $forward_fastq"
@@ -40,26 +43,46 @@ if [ ! "$#" -eq 4 ]; then
     exit 1
 fi
 
+#Indicate quality check complete
+echo "Control checks complete, files and arguments accepted"
+
 #Compute the reverse complements of each primer
 fp_complement=$(echo "$forward_primer" | tr ATGC[] TACG][ | rev)
 rp_complement=$(echo "$reverse_primer" | tr ATGC[] TACG][ | rev)
 
+#Indicate complements calculated
+echo "Primer compliments computed"
+
 #Infer reverse reads FASTQ file from forward reads FASTQ file
 reverse_fastq="$(echo "$forward_fastq" | grep  _R1_ | sed 's/R1/R2/' )"
 
+#Indicate reverse FASTQ inferred
+echo "Reverse reads FASTQ initiated"
+
 #Change output file name
 trimmed_fastq_f="$(basename "$forward_fastq" .fastq)-_trimmed.fastq"
+echo "$trimmed_fastq_f"
 trimmed_fastq_r="$(basename "$reverse_fastq" .fastq)-_trimmed.fastq"
+echo "$trimmed_fastq_r"
+
+#Indicate trimmed FASTQ arguments created
+echo "Trimmed sequences completed...assigning to directory"
 
 #Assign output file paths
 "$forward_fastq" > "$output_directory"/"$trimmed_fastq_f"
 "$reverse_fastq" > "$output_directory"/"$trimmed_fastq_r"
+
+#Indicate assignment complete
+echo "Directory assignment complete...calling Cutadapt"
 
 #Call Cutadapt
 cutadapt -a "$forward_primer"..."$rp_complement" \
     -A "$reverse_primer"..."$fp_complement" \
     --discard-untrimmed --pair-filter=any \
     -o "$trimmed_fastq_f" -p "$trimmed_fastq_r" "$forward_fastq" "$reverse_fastq"
+
+#Report completion of Cutadapt run
+echo "Cutadapt complete at..."
 
 #Follow up with date
 date
